18f26800e69639365e40838b6b15781854a53d6f
KillerZeus666 <katheryng2017@gmail.com>
2025-10-04 21:33:58 -0500

    Mejorando LibroUsuario

--- common/LibroUsuario.py
+++ common/LibroUsuario.py
@@ -1,14 +1,20 @@
+# common/LibroUsuario.py
+
 class LibroUsuario:
-    def __init__(self, codigo, titulo, autor, sede):
+    def __init__(self, codigo, titulo, autor, sede, ejemplares_disponibles, prestado):
         self.codigo = codigo
         self.titulo = titulo
         self.autor = autor
         self.sede = sede
+        self.ejemplares_disponibles = ejemplares_disponibles
+        self.prestado = prestado
 
     def to_dict(self):
         return {
             "codigo": self.codigo,
             "titulo": self.titulo,
             "autor": self.autor,
-            "sede": self.sede
+            "sede": self.sede,
+            "ejemplares_disponibles": self.ejemplares_disponibles,
+            "prestado": self.prestado
         }
